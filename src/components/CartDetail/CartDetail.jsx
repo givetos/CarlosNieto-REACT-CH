6983f3ae72e98dff55f1f9cf68abd2825ffb4150
Amadeo Isella <aisellamk@gmail.com>
2021-12-23 01:49:15 -0300

    Desafio Firebase II finalizado

--- src/components/CartDetail/CartDetail.jsx
+++ src/components/CartDetail/CartDetail.jsx
@@ -3,40 +3,36 @@ import { useDeleteFromCart } from "../../context/CartContext.jsx";
 import CartContext from "../../context/CartContext";
 import { Link } from "react-router-dom";
 
-const CartDetail = () => {
-  const { cart, calcularTotalPorItem } = useContext(CartContext);
+const CartDetail = ({ item }) => {
+  const { calcularTotalPorItem } = useContext(CartContext);
   const deleteProduct = useDeleteFromCart();
 
   return (
     <>
-      {cart.map(item => (
-        <div key={item.id}>
-          <div className="card mb-3">
-            <div className="row no-gutters">
-              <div className="col-md-2">
-                <Link to={`/item/${item.id}`}>
-                  <img src={item.img} className="card-img" alt={item.nombre} />
-                </Link>
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">{item.nombre}</h5>
-                  <p className="card-text">Price: $ {item.precio}</p>
-                  <p className="">Total: $ {calcularTotalPorItem(item)}</p>
-                  <p className="card-text">Genre: {item.generoId}</p>
-                  <p className="card-text">Quantity: {item.cantidad}</p>
-                  <button
-                    className="btn btn-secondary"
-                    onClick={() => deleteProduct(item)}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
+      <div className="card mb-3">
+        <div className="row no-gutters">
+          <div className="col-md-2">
+            <Link to={`/item/${item.id}`}>
+              <img src={item.img} className="card-img" alt={item.nombre} />
+            </Link>
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{item.nombre}</h5>
+              <p className="card-text">Price: $ {item.precio}</p>
+              <p className="">Total: $ {calcularTotalPorItem(item)}</p>
+              <p className="card-text">Genre: {item.generoId}</p>
+              <p className="card-text">Quantity: {item.cantidad}</p>
+              <button
+                className="btn btn-secondary"
+                onClick={() => deleteProduct(item)}
+              >
+                Delete
+              </button>
             </div>
           </div>
         </div>
-      ))}
+      </div>
     </>
   );
 };
--- src/components/Mensaje/Mensaje.jsx
+++ src/components/Mensaje/Mensaje.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 
 const Mensaje = ({ ord }) => {
-  const nombreprod = ord.items.map(x => x.nombre);
   const { buyer } = ord;
 
   return (
-    <div style={{ border: "1px solid black", margin: "30px", padding: "7px" }}>
-      <p>Purchase ID: {ord.id}</p>
-      <p>Date: {ord.date}</p>
-      <p>Product: {nombreprod}</p>
-      <p>Email: {buyer}</p>
-    </div>
+    <>
+      {ord.items.map(ticket => (
+        <div
+          style={{ border: "1px solid black", margin: "30px", padding: "7px" }}
+        >
+          <p>Purchase ID: {ord.id}</p>
+          <p>Date: {ord.date}</p>
+          <p>Product: {ticket.nombre}</p>
+          <p>Email: {buyer}</p>
+          <p>Total Price: ${ord.total}</p>
+        </div>
+      ))}
+    </>
   );
 };
 
